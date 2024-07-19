ddcb26226d9744ad7274db94d2ef4c39c2c83f7d
Hasdae <estevancalderaro@gmail.com>
2024-07-19 19:08:50 -0300

    Método construtor

---
+++ Modulo 3/Construtor.rb
@@ -0,0 +1,18 @@
+class Pessoa
+  def initialize(nome, idade) # Initialize é o construtor da classe
+    @nome = nome # Recebe a variavel com @ que está dentro do escopo da classe, se tornando um atributo da mesma
+    @idade = idade
+  end
+
+  def conferir
+    puts "Instância da classe iniciada com os valores:"
+    puts "Nome: #{@nome}"
+    puts "Idade: #{@idade} anos"
+  end
+end
+
+pessoa1 = Pessoa.new('Andre', 6)
+pessoa1.conferir
+
+pessoa2 = Pessoa.new('Gabriel', 13)
+pessoa2.conferir
