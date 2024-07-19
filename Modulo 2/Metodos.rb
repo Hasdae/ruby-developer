07e6b0a7ffdd60e5f72e0693fa21bb0bf0bb49cc
Hasdae <estevancalderaro@gmail.com>
2024-07-19 10:26:02 -0300

    Metodos

---
+++ Modulo 2/Metodos.rb
@@ -0,0 +1,12 @@
+def oi
+  puts "Oi"
+end
+
+2.times {oi}
+
+def bemVindo (nome, sobrenome)
+  puts "Olá, #{nome} #{sobrenome}! Seja bem vindo(a)!"
+end
+
+nome = ""
+bemVindo("Estevan", "Calderaro")
