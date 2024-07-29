c74c05d0df9286417365c8b5f9f8f2c5f91724a1
Hasdae <estevancalderaro@gmail.com>
2024-07-29 09:41:49 -0300

    feat: Generate home para substituir pagina inicial padrão

--- Modulo 4/blog/config/routes.rb
+++ Modulo 4/blog/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
+  root 'home#index'
   get "up" => "rails/health#show", as: :rails_health_check
-  root "articles#index"
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
   resources :posts
